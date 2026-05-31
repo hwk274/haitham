2b247d2fd7aec82651cc6d459e3b98f3318bfbde
hwk274 <hwk274@gmail.com>
2026-05-31 18:09:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,24 @@
-let score=0;
-let level=1;
-let i=0;
+fetch("data.json")
+.then(response => response.json())
+.then(data => {
 
-const data=[
-{emoji:"🐄",name:"البقرة"},
-{emoji:"🐴",name:"الحصان"},
-{emoji:"🐪",name:"الجمل"},
-{emoji:"🐦",name:"العصفور"}
-];
+    document.getElementById("siteName").textContent = data.siteName;
+    document.getElementById("siteDesc").textContent = data.description;
 
-function load(){
-document.getElementById("animal").innerText=data[i].emoji;
-document.getElementById("name").innerText=data[i].name;
-document.getElementById("msg").innerText="";
-}
+    document.getElementById("phone").textContent = data.phone;
+    document.getElementById("email").textContent = data.email;
 
-function speak(text){
-let s=new SpeechSynthesisUtterance(text);
-s.lang="ar-SA";
-speechSynthesis.speak(s);
-}
+    let featuresHTML = "";
 
-function answer(ok){
-if(ok){
-score++;
-document.getElementById("msg").innerText="🎉 ممتاز!";
-speak("ممتاز");
+    data.features.forEach(feature => {
+        featuresHTML += `
+        <div class="card">
+            <img src="${feature.image}">
+            <h3>${feature.title}</h3>
+            <p>${feature.description}</p>
+        </div>
+        `;
+    });
 
-if(score%3===0){
-level++;
-document.getElementById("level").innerText=level;
-}
-}else{
-document.getElementById("msg").innerText="❌ حاول مرة أخرى";
-speak("حاول مرة أخرى");
-}
-
-document.getElementById("score").innerText=score;
-}
-
-function next(){
-i=(i+1)%data.length;
-load();
-}
-
-load();
-
-/* زر التثبيت PWA */
-let deferredPrompt;
-const btn=document.getElementById("installBtn");
-
-window.addEventListener("beforeinstallprompt",(e)=>{
-e.preventDefault();
-deferredPrompt=e;
-btn.style.display="block";
-
-btn.addEventListener("click",()=>{
-deferredPrompt.prompt();
-deferredPrompt.userChoice.then(()=>{
-deferredPrompt=null;
-});
-});
+    document.getElementById("features").innerHTML = featuresHTML;
 });
